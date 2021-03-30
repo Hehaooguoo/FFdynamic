7bd04aff8a6fc0613603cc82c410f1ad05ba1fac
helinux <hehaooguoo@gmail.com>
2021-03-30 15:11:23 +0800

    修改pkg_config 环境变量

--- tools/dockerlize/ial_docker/run_ial.sh
+++ tools/dockerlize/ial_docker/run_ial.sh
@@ -1,4 +1,4 @@
-sudo docker run -tdi --net=host --rm --name iot_mcu  ffdynamic:mcu  /bin/bash
-# -v $PWD/../../../../FFdynamic:/FFdynamic
+sudo docker run -tdi --net=host --rm --name iot_mcu -v $PWD/../../../../FFdynamic:/FFdynamic  ffdynamic:mcu  /bin/bash
+# 
 # if you have nvidia card and driver installed
 # sudo docker run --device=/dev/nvidiactl --device=/dev/nvidia-uvm --device=/dev/nvidia0 -ti -v /usr/lib64/nvidia:/usr/lib64/nvidia --rm ffmpeg:nvi /bin/bash
